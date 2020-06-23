75621eb9f383eb631672abd4bef817e91f580e6d
Hieu Phan <64969360+Pogchop@users.noreply.github.com>
2020-06-23 20:28:33 +0700

    Add files via upload

---
+++ mmwf.js
@@ -0,0 +1,10 @@
+function showStyle(field) {
+field.style.backgroundColor='#FFFFCC';
+}
+function hideStyle(field) {
+field.style.backgroundColor='#FFFFFF';
+}
+function setFontStyle(field) {
+field.style.fontWeight='bold';
+field.style.fontFamily='Arial';
+}
